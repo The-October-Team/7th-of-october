a6d45c9d1d65a6160b8cca4e8b62ac0578b68962
Idan Carlebach <idanefrat@gmail.com>
2023-10-10 18:31:31 +0300

    Normal bootsrap components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
 import React from 'react'
 import './styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Button, Col, Image, Card } from 'react-bootstrap';
  
 function MainContent() {
     return (
       <div className="container mt-5">
-      <div className="row">
-        <div className="col-12">
+      <Row>
+        <Col className="col-12">
           <h1 className="text-center">Your Title</h1>
           <h2 className="text-center">Subtitle</h2>
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-12">
-          <img src="/images/graphic_design_is_my_passion.png" className="img-fluid" alt="Image Description" />
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-12">
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="col-12">
+          <Image src="/images/graphic_design_is_my_passion.png" className="img-fluid" alt="Image Description" />
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="col-12">
           <p className="text-center">Description text goes here. You can add more details below the image.</p>
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-6">
-          <a href="#" className="btn btn-primary btn-block">Button 1</a>
-        </div>
-        <div className="col-6">
-          <a href="#" className="btn btn-primary btn-block">Button 2</a>
-        </div>
-      </div>
+        </Col>
+      </Row>
+      <Container className="text-center">
+        <Row className="mt-3">
+          <Col>
+            <Button href="#" className="btn-primary btn-block" id="b1">Button 1</Button>
+          </Col>
+          <Col>
+            <Button href="#" className="btn-primary btn-block" id="b2">Button 2</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
     );
 }
