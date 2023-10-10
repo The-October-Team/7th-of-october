40390a5f61ec1d54bd0cbc38aaf46bf16e2a2f6c
SaarKeynan <44527391+SaarKeynan@users.noreply.github.com>
2023-10-10 17:49:36 +0300

    barebones design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import './styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+ 
+function MainContent() {
+    return (
+      <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="text-center">Your Title</h1>
+          <h2 className="text-center">Subtitle</h2>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-12">
+          <img src="/images/graphic_design_is_my_passion.png" className="img-fluid" alt="Image Description" />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-12">
+          <p className="text-center">Description text goes here. You can add more details below the image.</p>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-6">
+          <a href="#" className="btn btn-primary btn-block">Button 1</a>
+        </div>
+        <div className="col-6">
+          <a href="#" className="btn btn-primary btn-block">Button 2</a>
+        </div>
+      </div>
     </div>
-  );
+    );
 }
 
-export default App;
+const Homepage = () => {
+    return (
+        <>
+            <MainContent></MainContent>
+        </>
+    );
+}
+
+export default Homepage;
