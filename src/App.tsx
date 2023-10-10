66fd8d136be72c71d14d7ccee1f72341c4c1ba55
Etai Nadler <75544136+etainad008@users.noreply.github.com>
2023-10-10 19:44:53 +0300

    [Feature] Make homepage look normal (#2)
    * barebones design
    
    * Added colors and fonts to index.css
    
    * Added new line
    
    * Normal bootsrap components
    
    * Some changes
    
    * Now the code can be called pretty!
    
    ---------
    
    Co-authored-by: SaarKeynan <44527391+SaarKeynan@users.noreply.github.com>
    Co-authored-by: Idan Carlebach <idanefrat@gmail.com>
    Co-authored-by: Idan Carlebach <53697782+idandev@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,86 @@
-import React, { useState } from 'react';
-import './styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import eventsData from './data.json';
+import React, { useState } from "react";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Button, Col, Image } from "react-bootstrap";
+import eventsData from "./data.json";
 
 type SetEventIndex = (index: number) => void;
 
-function MainContent({ eventIndex, setEventIndex }: { eventIndex: number, setEventIndex: SetEventIndex }) {
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-12">
-          <h1 className="text-center">Your Title</h1>
-          <h2 className="text-center">Subtitle</h2>
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-12">
-          <img src={eventsData[eventIndex].path} className="img-fluid" alt="Image Description" />
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-12">
-          <p className="text-center">{eventsData[eventIndex].details}</p>
-        </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-6">
-          <a href="#" className="btn btn-primary btn-block" onClick={incIndex(eventIndex, setEventIndex)}>Button 1</a>
-        </div>
-        <div className="col-6">
-          <a href="#" className="btn btn-primary btn-block">Button 2</a>
-        </div>
-      </div>
-    </div>
-  );
+function MainContent({
+    eventIndex,
+    setEventIndex,
+}: {
+    eventIndex: number;
+    setEventIndex: SetEventIndex;
+}) {
+    return (
+        <Container className="mt-5">
+            <Row>
+                <Col className="col-12">
+                    <h1 className="text-center">Your Title</h1>
+                    <h2 className="text-center">Subtitle</h2>
+                </Col>
+            </Row>
+            <Row className="mt-3">
+                <Col className="col-12">
+                    <Image
+                        src={eventsData[eventIndex].path}
+                        className="img-fluid"
+                        alt="Image Description"
+                    />
+                </Col>
+            </Row>
+            <Row className="mt-3">
+                <Col className="col-12">
+                    <p className="text-center">
+                        {eventsData[eventIndex].details}
+                    </p>
+                </Col>
+            </Row>
+            <Container className="text-center">
+                <Row className="mt-3">
+                    <Col>
+                        <Button
+                            href="#"
+                            className="btn-primary btn-block"
+                            onClick={incIndex(eventIndex, setEventIndex)}
+                            id="b1"
+                        >
+                            Button 1
+                        </Button>
+                    </Col>
+                    <Col>
+                        <Button
+                            href="#"
+                            className="btn-primary btn-block"
+                            id="b2"
+                        >
+                            Button 2
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
 }
 
 function incIndex(eventIndex: number, setEventIndex: SetEventIndex) {
-  return () => {
-    if (eventIndex + 1 >= eventsData.length)
-      return setEventIndex(0);
-    setEventIndex(eventIndex + 1);
-  }
+    return () => {
+        if (eventIndex + 1 >= eventsData.length) return setEventIndex(0);
+        setEventIndex(eventIndex + 1);
+    };
 }
 
 const Homepage = () => {
-  let [eventIndex, setEventIndex] = useState(0);
-  return (
-    <>
-      <MainContent eventIndex={eventIndex} setEventIndex={setEventIndex}></MainContent>
-    </>
-  );
-}
+    const [eventIndex, setEventIndex] = useState(0);
+    return (
+        <>
+            <MainContent
+                eventIndex={eventIndex}
+                setEventIndex={setEventIndex}
+            ></MainContent>
+        </>
+    );
+};
 
 export default Homepage;
