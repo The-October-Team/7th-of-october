64a57164c8957d6df352592354bb7cd56bad6256
shuruk421 <32776970+shuruk421@users.noreply.github.com>
2023-10-12 16:25:31 +0300

    placeholder links

--- src/pages/enough.tsx
+++ src/pages/enough.tsx
@@ -9,8 +9,8 @@ function EnoughContent() {
             <ul className="bullet-points">
                 <li> SUPPORT ISRAEL ONLINE USING THE HASHTAGS <span className="hashtag"> #TRUEFACEOFPALESTINE #HAMASisISIS </span ></li>
                 <li> SHARE THIS SITE TO SHOW THE WORLD THE ATROCITIES THE ISRAELI PEOPLE FACE</li>
-                <li> READ ABOUT <a> THE CONFLICT </a> AND HOW PALESTINIAN LEADERSHIP ALWAYS DESTROYS THE PEACE PROCESS </li>
-                <li> DONATE TO THE FOLLOWING <a> FUNDRAISERS </a> SUPPORTING THE SURVIVORS AND THE IDF </li>
+                <li> READ ABOUT <a href="https://en.wikipedia.org/wiki/Israeli%E2%80%93Palestinian_conflict"> THE CONFLICT </a> AND HOW PALESTINIAN LEADERSHIP ALWAYS DESTROYS THE PEACE PROCESS </li>
+                <li> DONATE TO THE FOLLOWING <a href="https://google.com/"> FUNDRAISERS </a> SUPPORTING THE SURVIVORS AND THE IDF </li>
                 <li> DON&apos;T FALL FOR THE RADICAL ISLAMIST PROPOGANDA, CHOOSE <u> THE RIGHT SIDE OF HISTORY </u> BEFORE MORE JEWISH BLOOD IS SPILT </li>
             </ul>
             <img src="../images/blood.png" id="blood-img" />
