eb62bf06af8ed36bf339266ad14f107db0a32701
shuruk421 <32776970+shuruk421@users.noreply.github.com>
2023-10-12 18:44:45 +0300

    added share buttons

--- src/pages/enough.tsx
+++ src/pages/enough.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import { FacebookShareButton, TwitterShareButton, WhatsappIcon, WhatsappShareButton } from "react-share";
+import { FacebookIcon, TwitterIcon } from "react-share";
 
 function EnoughContent() {
     return (
@@ -9,7 +11,29 @@ function EnoughContent() {
             <p className="title">WHAT CAN I DO?</p>
             <ul className="bullet-points">
                 <li> SUPPORT ISRAEL ONLINE USING THE HASHTAGS <span className="hashtag"> #TRUEFACEOFPALESTINE #HAMASisISIS </span ></li>
-                <li> SHARE THIS SITE TO SHOW THE WORLD THE ATROCITIES THE ISRAELI PEOPLE FACE</li>
+                <li> SHARE THIS SITE TO SHOW THE WORLD THE ATROCITIES THE ISRAELI PEOPLE FACE <br/>
+                    <FacebookShareButton
+                        url={"https://trueface.com/"}
+                        quote={"Learn more about how Hamas Is Isis!"}
+                        hashtag={"#TRUEFACEOFPALESTINE"}
+                        className="share-button"
+                    >
+                        <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                    <TwitterShareButton
+                        title={"The true face of Palestine"}
+                        url={"https://trueface.com/"}
+                        hashtags={["TRUEFACEOFPALESTINE", "HAMASisISIS"]}
+                    >
+                        <TwitterIcon size={32} round />
+                    </TwitterShareButton>
+                    <WhatsappShareButton
+                        title={"The true face of Palestine"}
+                        url={"https://trueface.com/"}
+                    >
+                        <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
+                </li>
                 <li> READ ABOUT <a href="https://en.wikipedia.org/wiki/Israeli%E2%80%93Palestinian_conflict"> THE CONFLICT </a> AND HOW PALESTINIAN LEADERSHIP ALWAYS DESTROYS THE PEACE PROCESS </li>
                 <li> DONATE TO THE FOLLOWING <a href="https://google.com/"> FUNDRAISERS </a> SUPPORTING THE SURVIVORS AND THE IDF </li>
                 <li> DON&apos;T FALL FOR THE RADICAL ISLAMIST PROPOGANDA, CHOOSE <u> THE RIGHT SIDE OF HISTORY </u> BEFORE MORE JEWISH BLOOD IS SPILT </li>
