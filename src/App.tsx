d19f0c1351892c0ab793e9c027edfebe85559192
shuruk421 <32776970+shuruk421@users.noreply.github.com>
2023-10-14 01:31:13 +0300

    alot

--- src/App.tsx
+++ src/App.tsx
@@ -3,79 +3,64 @@ import "./styles.css";
 import eventsData from "./data.json";
 
 type SetEventIndex = (index: number) => void;
+type SetContentWarning = (index: boolean) => void;
 
 function MainContent({
     eventIndex,
+    contentWarning,
     setEventIndex,
+    setContentWarning
 }: {
     eventIndex: number;
+    contentWarning: boolean;
     setEventIndex: SetEventIndex;
+    setContentWarning: SetContentWarning;
 }) {
     return (
         <div id="page-container">
+            <img src="../images/blood-2.png" id="blood-img-2" alt="blood" />
             <section id="title-container">
-                <p id="main-title">Your Title</p>
-                <p id="main-subtitle">Subtitle</p>
+                <p id="main-title">TRUE FACE <br /> OF PALESTINE </p>
+                <p id="main-subtitle">IN THE PAST FEW DAYS, HAMAS TERRORISTS TOOK OVER ISRAELI TOWNS. THE IMAGES BELOW ARE THE AFTERMATH OF THEIR BRUTAL ATTACK.</p>
             </section>
             <main id="graphic-container">
+                {contentWarning &&
+                    <div id="blur" />}
+                {contentWarning &&
+                    <div id="warning-containter">
+                        <img id="eye" src="../images/eye.png" />
+                        <p
+                            id="content-warning"
+                            onClick={disableWarning(contentWarning, setContentWarning)}
+                        >
+                            CONTENT WARNING: EXTREME VIOLENCE
+                            <br />
+                            SHOW
+                        </p>
+                    </div>}
+                <div id="fade-top" />
                 <img
                     src={eventsData[eventIndex].path}
                     id="graphic"
                     alt="Image Description"
                 />
+                <div id="fade-bottom" />
             </main>
             <p id="graphic-detail">
                 {eventsData[eventIndex].details}
             </p>
             <div className="btn-container">
                 <button
-                    id="btn-ive-seen-enough"
-                >
-                    I&apos;ve seen enough.
-                </button>
-                <button
                     onClick={incIndex(eventIndex, setEventIndex)}
                     id="btn-show-me-more"
                 >
-                    Show me more
+                    SHOW ME MORE
                 </button>
-            </div>
-            <div id="NSFL-container">
-                <input type="checkbox" id="nsfw-toggle" />
-                <label htmlFor="nsfw-toggle" id="nsfw-label">
-                    <svg
-                        id="warning"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M12 15H12.01M12 12V9M4.98207 19H19.0179C20.5615 19 21.5233 17.3256 20.7455 15.9923L13.7276 3.96153C12.9558 2.63852 11.0442 2.63852 10.2724 3.96153L3.25452 15.9923C2.47675 17.3256 3.43849 19 4.98207 19Z"
-                            stroke="#0f0f0f"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        />
-                    </svg>
-
-                    <svg
-                        id="lock"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288"
-                            stroke="#0f0f0f"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        />
-                    </svg>
-                </label>                    
-                <p id="NSFL-text">
-                    NSFL
-                </p>
+                <button
+                    id="btn-ive-seen-enough"
+                >
+                    I&apos;VE SEEN ENOUGH
+                </button>
             </div>
         </div>
     );
@@ -89,11 +74,19 @@ function incIndex(eventIndex: number, setEventIndex: SetEventIndex) {
     };
 }
 
+function disableWarning(contentWarning: boolean, setContentWarning: SetContentWarning) {
+    return () => {
+        setContentWarning(false);
+    };
+}
+
 const Homepage = () => {
     const [eventIndex, setEventIndex] = useState(0);
+    const [contentWarning, setContentWarning] = useState(true);
+
     return (
         <>
-            <MainContent eventIndex={eventIndex} setEventIndex={setEventIndex}></MainContent>
+            <MainContent eventIndex={eventIndex} contentWarning={contentWarning} setContentWarning={setContentWarning} setEventIndex={setEventIndex}></MainContent>
         </>
     );
 };
