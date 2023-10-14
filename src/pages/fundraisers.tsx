1457361d27cb0666624dcc12214f98e321770d10
Etai Nadler <75544136+etainad008@users.noreply.github.com>
2023-10-14 19:52:43 +0300

    Designed the fundraising page

--- src/pages/fundraisers.tsx
+++ src/pages/fundraisers.tsx
@@ -1,34 +1,122 @@
 import React from "react";
 import "../styles.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "../fundraisers.css";
 import { Link } from "react-router-dom";
 import { ReactComponent as HouseIcon } from "../images/house.svg";
 
 function FundraisersContent() {
     return (
         <div className="fundraisers">
-            <p className="title">SUPPORT ISRAEL</p>
-            <p className="subtitle">Donate to Hospitals and Medical Centers</p>
-            <p className="donate-link"><a href="https://www.bmc.gov.il/?CategoryID=511"> Barzilai Hospital </a> </p>
-            <p className="donate-link"><a href="https://hadassah.gospringboard.com/"> Hadassah Hospital </a> </p>
-            <p className="donate-link"><a href="https://www.soroka.org/donate/"> Soroka Medical Center </a> </p>
-            <p className="donate-link"><a href="https://give.afsmc.org/give/409953/#!/donation/checkout"> Sheba Medical Center </a> </p>
-            <p className="subtitle">Donate to Emergency Services</p>
-            <p className="donate-link"><a href="https://www.mdais.org/en/donation"> Magen David Adom </a> </p>
-            <p className="donate-link"><a href="https://israelrescue.org/"> United Hatzalah </a> </p>
-            <p className="donate-link"><a href="https://give.zakaworld.org/"> Zaka </a> </p>
-            <p className="subtitle">Support Soldiers and Families</p>
-            <p className="donate-link"><a href="https://belevechad.nyc/who-we-are/"> Belev Echad </a> </p>
-            <p className="donate-link"><a href="https://www.fidf.org/"> Friends of the IDF </a> </p>
-            <p className="donate-link"><a href="https://www.latet.org.il/en/"> Latet  </a> </p>
-            <p className="subtitle">Support Mental Health Resources</p>
-            <p className="donate-link"><a href="https://www.afnatal.org/"> AFNATAL </a> </p>
-            <p className="donate-link"><a href="https://en.eran.org.il/"> ERAN Emotional First Aid </a> </p>
-            <p className="donate-link"><a href="https://onefamilyfundus.org/"> One Family Fund </a> </p>
-            <p className="donate-link"><a href="https://www.bayit-cham.org.il/en/a-warm-house-%f0%9f%92%97-brightens-the-heart/"> A Warm House </a> </p>
-            <p className="donate-link"><a href="https://www.israaid.org/"> IsraAID </a> </p>
+            <p className="title"><span>SUPPORT</span> ISRAEL</p>
+            <section>
+                <p className="subtitle">
+                    Donate to Hospitals and Medical Centers
+                </p>
+                <div className="fundraiser-category">
+                    <a
+                        className="donate-link"
+                        href="https://www.bmc.gov.il/?CategoryID=511"
+                    >
+                        Barzilai Hospital
+                    </a>
+
+                    <a
+                        className="donate-link"
+                        href="https://hadassah.gospringboard.com/"
+                    >
+                        Hadassah Hospital
+                    </a>
+
+                    <a
+                        className="donate-link"
+                        href="https://www.soroka.org/donate/"
+                    >
+                        Soroka Medical Center
+                    </a>
+
+                    <a
+                        className="donate-link"
+                        href="https://give.afsmc.org/give/409953/#!/donation/checkout"
+                    >
+                        Sheba Medical Center
+                    </a>
+                </div>
+            </section>
+            <section>
+                <p className="subtitle">Donate to Emergency Services</p>
+                <div className="fundraiser-category">
+                    <a
+                        className="donate-link"
+                        href="https://www.mdais.org/en/donation"
+                    >
+                        Magen David Adom
+                    </a>
+                    <a className="donate-link" href="https://israelrescue.org/">
+                        United Hatzalah
+                    </a>{" "}
+                    <a
+                        className="donate-link"
+                        href="https://give.zakaworld.org/"
+                    >
+                        Zaka
+                    </a>
+                </div>
+            </section>{" "}
+            <section>
+                <p className="subtitle">Support Soldiers and Families</p>
+                <div className="fundraiser-category">
+                    <a
+                        className="donate-link"
+                        href="https://belevechad.nyc/who-we-are/"
+                    >
+                        Belev Echad
+                    </a>
+
+                    <a className="donate-link" href="https://www.fidf.org/">
+                        Friends of the IDF
+                    </a>
+
+                    <a
+                        className="donate-link"
+                        href="https://www.latet.org.il/en/"
+                    >
+                        Latet{" "}
+                    </a>
+                </div>
+            </section>{" "}
+            <section>
+                <p className="subtitle">Support Mental Health Resources</p>
+                <div className="fundraiser-category">
+                    <a className="donate-link" href="https://www.afnatal.org/">
+                        AFNATAL
+                    </a>
+
+                    <a className="donate-link" href="https://en.eran.org.il/">
+                        ERAN Emotional First Aid
+                    </a>
+
+                    <a
+                        className="donate-link"
+                        href="https://onefamilyfundus.org/"
+                    >
+                        One Family Fund
+                    </a>
+
+                    <a
+                        className="donate-link"
+                        href="https://www.bayit-cham.org.il/en/a-warm-house-%f0%9f%92%97-brightens-the-heart/"
+                    >
+                        A Warm House
+                    </a>
+
+                    <a className="donate-link" href="https://www.israaid.org/">
+                        IsraAID
+                    </a>
+                </div>
+            </section>
             <img src="../images/Star_of_David.png" id="star-of-david" />
-            <Link to='/'>
+            <Link to="/">
                 <HouseIcon width={40} height={40} id="house-icon" />
             </Link>
         </div>
